9b0f335491649c5c1293ecb0bab58b5680f018d9
Oren (electricessence) <oren@electrified.net>
2019-05-27 15:44:42 -0700

    Updated reference.

--- AsyncTextWriter.cs
+++ AsyncTextWriter.cs
@@ -1,74 +1,85 @@
 ﻿using Open.ChannelExtensions;
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace RegexLineExtractor
 {
-    public class AsyncLineWriter : IDisposable
-    {
-        readonly Channel<string> _channel;
+	public class AsyncLineWriter : IDisposable
+	{
+		readonly Channel<string> _channel;
 
-        /// <summary>
-        /// Constructs an AsyncTextWriter.
-        /// </summary>
-        /// <param name="fileName">An optional file name.  If the name is null, then this will act as a dummy writer.</param>
-        public AsyncLineWriter(string fileName = null)
-        {
-            if (fileName == null)
-            {
-                Completion = Task.CompletedTask;
-            }
-            else
-            {
-                var file = new Lazy<StreamWriter>(() => new StreamWriter(fileName));
+		/// <summary>
+		/// Constructs an AsyncTextWriter.
+		/// </summary>
+		/// <param name="fileName">An optional file name.  If the name is null, then this will act as a dummy writer.</param>
+		public AsyncLineWriter(string fileName = null)
+		{
+			if (fileName == null)
+			{
+				Completion = Task.CompletedTask;
+			}
+			else
+			{
+				var file = new Lazy<StreamWriter>(() => new StreamWriter(fileName));
 
-                _channel = Channel.CreateBounded<string>(new BoundedChannelOptions(100)
-                {
-                    SingleReader = true,
-                    SingleWriter = false,
-                    AllowSynchronousContinuations = true
-                });
+				_channel = Channel.CreateBounded<string>(new BoundedChannelOptions(100)
+				{
+					SingleReader = true,
+					SingleWriter = false,
+					AllowSynchronousContinuations = true
+				});
 
-                Completion = _channel
-                    .ReadAllAsync(line => new ValueTask(file.Value.WriteLineAsync(line)))
-                    .AsTask()
-                    .ContinueWith(t =>
-                    {
-                        if (t.IsFaulted)
-                        {
-                            _channel.Writer.TryComplete(t.Exception);
-                        }
+				Completion = _channel
+					.ReadAllAsync(line => new ValueTask(file.Value.WriteLineAsync(line)))
+					.AsTask()
+					.ContinueWith(t =>
+					{
+						if (t.IsFaulted)
+						{
+							_channel.Writer
+								.TryComplete(t.Exception);
+						}
 
-                        if (file.IsValueCreated)
-                        {
-                            file.Value
-                                .FlushAsync() // Async flush, then sync dispose.
-                                .ContinueWith(f => file.Value.Dispose());
-                        }
-                        return t;
-                    })
-                    .Unwrap();
-            }
-        }
+						if (!file.IsValueCreated)
+							return t;
 
-        public ValueTask WriteAsync(string line)
-            => _channel == null ? new ValueTask()
-                : _channel.Writer.WriteIfOpenAsync(line);
+						return file
+							.Value
+							.FlushAsync() // Async flush, then sync dispose.
+							.ContinueWith(
+								f =>
+								{
+									file.Value.Dispose();
+									return t;
+								},
+								TaskContinuationOptions.ExecuteSynchronously)
+							.Unwrap();
 
-        public Task Completion { get; }
+					}, TaskContinuationOptions.ExecuteSynchronously)
+					.Unwrap();
+			}
+		}
 
-        public Task CompleteAsync()
-        {
-            _channel?.Writer.TryComplete();
-            return Completion;
-        }
+		public ValueTask WriteAsync(string line, CancellationToken cancellationToken = default)
+			=> _channel == null || _channel.Writer.TryWrite(line)
+				? new ValueTask()
+				: _channel.Writer.WriteAsync(line, cancellationToken);
 
-        public ValueTask DisposeAsync()
-            => new ValueTask(CompleteAsync());
+		public Task Completion { get; }
 
-        public void Dispose()
-            => CompleteAsync().Wait();
-    }
+		public Task CompleteAsync()
+		{
+			_channel?.Writer.TryComplete();
+			return Completion;
+		}
+
+		public ValueTask DisposeAsync()
+			=> new ValueTask(CompleteAsync());
+
+		public void Dispose()
+			=> CompleteAsync().Wait();
+	}
 }
--- Program.cs
+++ Program.cs
@@ -9,100 +9,95 @@ using Open.ChannelExtensions;
 
 namespace RegexLineExtractor
 {
-    class Program
-    {
-        const string OutputGroupName = "output";
-        const string ResultsDirectoryName = "results";
+	class Program
+	{
+		const string OutputGroupName = "output";
+		const string ResultsDirectoryName = "results";
 
-        static async Task Main(string[] args)
-        {
-            // argument 1: patterns
-            var patternsFileName = args.ElementAtOrDefault(1) ?? "patterns.regex";
-            var patterns = File
-                .ReadLines(patternsFileName)
-                .Select((p, i) =>
-                {
-                    var regex = new Regex(p, RegexOptions.Compiled);
-                    return (
-                        pattern: regex,
-                        hasOutput: regex.GetGroupNames().Contains(OutputGroupName),
-                        writer: new AsyncLineWriter($"results/pattern-{i + 1}.lines.txt")
-                    );
-                })
-                .ToArray();
+		static async Task Main(string[] args)
+		{
+			// argument 1: patterns
+			var patternsFileName = args.ElementAtOrDefault(1) ?? "patterns.regex";
+			var patterns = File
+				.ReadLines(patternsFileName)
+				.Select((p, i) =>
+				{
+					var regex = new Regex(p, RegexOptions.Compiled);
+					return (
+						pattern: regex,
+						hasOutput: regex.GetGroupNames().Contains(OutputGroupName),
+						writer: new AsyncLineWriter($"results/pattern-{i + 1}.lines.txt")
+					);
+				})
+				.ToArray();
 
-            // Make sure we can dump our results.
-            if (!Directory.Exists(ResultsDirectoryName))
-                Directory.CreateDirectory(ResultsDirectoryName);
+			// Make sure we can dump our results.
+			if (!Directory.Exists(ResultsDirectoryName))
+				Directory.CreateDirectory(ResultsDirectoryName);
 
-            // Clean any previous runs.
-            foreach(var file in Directory.GetFiles(ResultsDirectoryName))
-                File.Delete(file);               
+			// Clean any previous runs.
+			foreach(var file in Directory.GetFiles(ResultsDirectoryName))
+				File.Delete(file);
 
-            try
-            {
-                var sw = Stopwatch.StartNew();
-                using (var notMatched = new AsyncLineWriter($"results/not-matched.lines.txt"))
-                {
-                    var matchCount = 0;
-                    using (var source = new StreamReader(args[0])) // argument 0: source
-                    {
-                        var lineCount = 0;
-                        await source
-                            .ToChannel(1000)
-                            .ReadAllConcurrentlyAsync(4, async line =>
-                            {
-                                var found = false;
-                                foreach (var (pattern, hasOutput, writer) in patterns)
-                                {
-                                    var m = pattern.Match(line);
-                                    if (!m.Success)
-                                        continue;
+			try
+			{
+				var sw = Stopwatch.StartNew();
+				using (var notMatched = new AsyncLineWriter($"results/not-matched.lines.txt"))
+				{
+					var matchCount = 0;
+					using (var source = new StreamReader(args[0])) // argument 0: source
+					{
+						var lineCount = 0;
+						await source
+							.ToChannel(100)
+							.ReadAllConcurrentlyAsync(4, async line =>
+							{
+								var found = false;
+								foreach (var (pattern, hasOutput, writer) in patterns)
+								{
+									var m = pattern.Match(line);
+									if (!m.Success)
+										continue;
 
-                                    if (hasOutput)
-                                    {
-                                        var o = m.Groups[OutputGroupName];
-                                        if (!o.Success)
-                                            continue;
+									if (hasOutput)
+									{
+										var o = m.Groups[OutputGroupName];
+										if (!o.Success)
+											continue;
 
-                                        line = o.Value;
-                                    }
+										line = o.Value;
+									}
 
-                                    var ok = writer.WriteAsync(line);
-                                    if (!ok.IsCompletedSuccessfully)
-                                        await ok;
+									await writer.WriteAsync(line);
 
-                                    Interlocked.Increment(ref matchCount);
-                                    found = true;
-                                }
+									Interlocked.Increment(ref matchCount);
+									found = true;
+									break;
+								}
 
-                                if (!found)
-                                {
-                                    var s = notMatched.WriteAsync(line);
-                                    if (!s.IsCompletedSuccessfully)
-                                        await s;
-                                }
+								if (!found)
+									await notMatched.WriteAsync(line);
+  
+								var count = Interlocked.Increment(ref lineCount);
+								if (count % 1000 == 0)
+									lock (sw) Console.WriteLine("Lines Processed: {0:N0}", count);
+							});
 
-                                var count = Interlocked.Increment(ref lineCount);
-                                if (count % 1000 == 0)
-                                    lock (sw) Console.WriteLine("Lines Processed: {0:N0}", count);
-                            });
+						sw.Stop();
+					}
 
-                        sw.Stop();
-                    }
+					Console.WriteLine("Done: {0:N3} seconds", sw.Elapsed.TotalSeconds);
+					Console.WriteLine("{0:N0} matches Found.", matchCount);
 
-                    Console.WriteLine("Done: {0:N3} seconds", sw.Elapsed.TotalSeconds);
-                    Console.WriteLine("{0:N0} matches Found.", matchCount);
-
-                    await Task.WhenAll(patterns.Select(p => p.writer.CompleteAsync()));
-                    await notMatched.CompleteAsync();
-                }
-            }
-            finally
-            {
-                foreach (var (pattern, hasOutput, writer) in patterns)
-                    await writer.DisposeAsync();
-            }
-        }
-    }
+					await Task.WhenAll(patterns.Select(p => p.writer.CompleteAsync()));
+					await notMatched.CompleteAsync();
+				}
+			}
+			finally
+			{
+				foreach (var (pattern, hasOutput, writer) in patterns)
+					await writer.DisposeAsync();
+			}
+		}
+	}
 }
