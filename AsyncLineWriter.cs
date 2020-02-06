48dd625592a2877bf1b152672ae0b7cdb305816d
Oren (electricessence) <oren@electrified.net>
2020-02-06 00:24:40 -0800

    Cleanup.

--- AsyncLineWriter.cs
+++ AsyncLineWriter.cs
@@ -7,84 +7,84 @@ using System.Threading.Tasks;
 
 namespace CsvLineValidator
 {
-    public class AsyncLineWriter : IAsyncDisposable
-    {
-        readonly Channel<ReadOnlyMemory<char>>? _channel;
+	public class AsyncLineWriter : IAsyncDisposable
+	{
+		readonly Channel<ReadOnlyMemory<char>>? _channel;
 
-        /// <summary>
-        /// Constructs an AsyncTextWriter.
-        /// </summary>
-        /// <param name="fileName">An optional file name.  If the name is null, then this will act as a dummy writer.</param>
-        public AsyncLineWriter(string? fileName = null, string? headerRow = null)
-        {
-            if (fileName == null)
-            {
-                Completion = Task.CompletedTask;
-            }
-            else
-            {
-                var file = new Lazy<StreamWriter>(() =>
-                {
-                    var sr = new StreamWriter(fileName);
-                    if (headerRow != null) sr.WriteLine(headerRow);
-                    return sr;
-                });
+		/// <summary>
+		/// Constructs an AsyncTextWriter.
+		/// </summary>
+		/// <param name="fileName">An optional file name.  If the name is null, then this will act as a dummy writer.</param>
+		public AsyncLineWriter(string? fileName = null, string? headerRow = null)
+		{
+			if (fileName == null)
+			{
+				Completion = Task.CompletedTask;
+			}
+			else
+			{
+				var file = new Lazy<StreamWriter>(() =>
+				{
+					var sr = new StreamWriter(fileName);
+					if (headerRow != null) sr.WriteLine(headerRow);
+					return sr;
+				});
 
-                _channel = Channel.CreateBounded<ReadOnlyMemory<char>>(new BoundedChannelOptions(100)
-                {
-                    SingleReader = true,
-                    SingleWriter = false,
-                    AllowSynchronousContinuations = true
-                });
+				_channel = Channel.CreateBounded<ReadOnlyMemory<char>>(new BoundedChannelOptions(100)
+				{
+					SingleReader = true,
+					SingleWriter = false,
+					AllowSynchronousContinuations = true
+				});
 
-                Completion = _channel
-                    .ReadAllAsync(line => new ValueTask(file.Value.WriteLineAsync(line)))
-                    .AsTask()
-                    .ContinueWith(t =>
-                    {
-                        if (t.IsFaulted)
-                        {
-                            _channel.Writer
-                                .TryComplete(t.Exception);
-                        }
+				Completion = _channel
+					.ReadAllAsync(line => new ValueTask(file.Value.WriteLineAsync(line)))
+					.AsTask()
+					.ContinueWith(t =>
+					{
+						if (t.IsFaulted)
+						{
+							_channel.Writer
+								.TryComplete(t.Exception);
+						}
 
-                        if (!file.IsValueCreated)
-                            return t;
+						if (!file.IsValueCreated)
+							return t;
 
-                        return file
-                            .Value
-                            .FlushAsync() // Async flush, then sync dispose.
-                            .ContinueWith(async
-                                f =>
-                                {
-                                    await file.Value.DisposeAsync();
-                                    return t;
-                                },
-                                TaskContinuationOptions.ExecuteSynchronously)
-                            .Unwrap();
+						return file
+							.Value
+							.FlushAsync() // Async flush, then sync dispose.
+							.ContinueWith(async
+								f =>
+								{
+									await file.Value.DisposeAsync();
+									return t;
+								},
+								TaskContinuationOptions.ExecuteSynchronously)
+							.Unwrap();
 
-                    }, TaskContinuationOptions.ExecuteSynchronously)
-                    .Unwrap();
-            }
-        }
+					}, TaskContinuationOptions.ExecuteSynchronously)
+					.Unwrap();
+			}
+		}
 
-        public ValueTask WriteAsync(ReadOnlyMemory<char> line, CancellationToken cancellationToken = default)
-            => _channel == null || _channel.Writer.TryWrite(line)
-                ? new ValueTask()
-                : _channel.Writer.WriteAsync(line, cancellationToken);
+		public ValueTask WriteAsync(ReadOnlyMemory<char> line, CancellationToken cancellationToken = default)
+			=> _channel == null || _channel.Writer.TryWrite(line)
+				? new ValueTask()
+				: _channel.Writer.WriteAsync(line, cancellationToken);
 
 		public ValueTask WriteAsync(string line, CancellationToken cancellationToken = default)
 			=> WriteAsync(line.AsMemory(), cancellationToken);
 
-        public Task Completion { get; }
+		public Task Completion { get; }
 
-        public Task CompleteAsync()
-        {
-            _channel?.Writer.TryComplete();
-            return Completion;
-        }
+		public Task CompleteAsync()
+		{
+			_channel?.Writer.TryComplete();
+			return Completion;
+		}
 
-        public ValueTask DisposeAsync()
-            => new ValueTask(CompleteAsync());
-    }
+		public ValueTask DisposeAsync()
+			=> new ValueTask(CompleteAsync());
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,104 +1,104 @@
-﻿using System;
+﻿using Open.ChannelExtensions;
+using Open.Text.CSV;
+using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Net.Http;
-using Open.ChannelExtensions;
-using Open.Text.CSV;
-using System.Net;
 
 namespace CsvLineValidator
 {
-    class Program
-    {
-        const string ResultsDirectoryName = "results";
+	class Program
+	{
+		const string ResultsDirectoryName = "results";
 
-        // dotnet run filename.csv column urlPrefix
-        static async Task Main(string[] args)
-        {
-            var filePath = args.ElementAtOrDefault(0);
-            if (string.IsNullOrWhiteSpace(filePath)) filePath = "urls.csv";
-            if (!filePath.EndsWith(".csv", StringComparison.InvariantCultureIgnoreCase))
-                throw new Exception("Must be a valid .csv file name");
-            var columnName = args.ElementAtOrDefault(1);
-            if (string.IsNullOrWhiteSpace(columnName)) columnName = "0";
-            string? prefix = args.ElementAtOrDefault(2);
-            if (string.IsNullOrWhiteSpace(columnName)) prefix = null;
+		// dotnet run filename.csv column urlPrefix
+		static async Task Main(string[] args)
+		{
+			var filePath = args.ElementAtOrDefault(0);
+			if (string.IsNullOrWhiteSpace(filePath)) filePath = "urls.csv";
+			if (!filePath.EndsWith(".csv", StringComparison.InvariantCultureIgnoreCase))
+				throw new Exception("Must be a valid .csv file name");
+			var columnName = args.ElementAtOrDefault(1);
+			if (string.IsNullOrWhiteSpace(columnName)) columnName = "0";
+			string? prefix = args.ElementAtOrDefault(2);
+			if (string.IsNullOrWhiteSpace(columnName)) prefix = null;
 
-            using var source = new StreamReader(filePath); // argument 0: source
-            var firstLine = await source.ReadLineAsync();
-            if (firstLine == null) return;
+			using var source = new StreamReader(filePath); // argument 0: source
+			var firstLine = await source.ReadLineAsync();
+			if (firstLine == null) return;
 
-            if (!int.TryParse(columnName, out var columnIndex))
-            {
-                var firstLineValues = CsvUtility.GetLine(firstLine).ToArray();
-                columnIndex = Array.IndexOf(firstLineValues, columnName);
-                if (columnIndex == -1) throw new Exception("Column name not found.");
-            }
+			if (!int.TryParse(columnName, out var columnIndex))
+			{
+				var firstLineValues = CsvUtility.GetLine(firstLine).ToArray();
+				columnIndex = Array.IndexOf(firstLineValues, columnName);
+				if (columnIndex == -1) throw new Exception("Column name not found.");
+			}
 
 			#region Pre-Cleanup
-            // Make sure we can dump our results.
-            if (!Directory.Exists(ResultsDirectoryName))
-                Directory.CreateDirectory(ResultsDirectoryName);
+			// Make sure we can dump our results.
+			if (!Directory.Exists(ResultsDirectoryName))
+				Directory.CreateDirectory(ResultsDirectoryName);
 
-            // Clean any previous runs.
-            foreach (var file in Directory.GetFiles(ResultsDirectoryName))
-                File.Delete(file);
+			// Clean any previous runs.
+			foreach (var file in Directory.GetFiles(ResultsDirectoryName))
+				File.Delete(file);
 			#endregion
 
-            var writers = new ConcurrentDictionary<string, AsyncLineWriter>();
-            #region Helpers
-            AsyncLineWriter GetLineWriterFor(string name)
-                => writers.GetOrAdd(name, key => new AsyncLineWriter($"{ResultsDirectoryName}/{key}.csv", firstLine));
-            AsyncLineWriter GetLineWriterForCode(HttpStatusCode code)
-                => GetLineWriterFor((int)code + "-" + code.ToString());
-            #endregion
+			var writers = new ConcurrentDictionary<string, AsyncLineWriter>();
+			#region Helpers
+			AsyncLineWriter GetLineWriterFor(string name)
+				=> writers.GetOrAdd(name, key => new AsyncLineWriter($"{ResultsDirectoryName}/{key}.csv", firstLine));
+			AsyncLineWriter GetLineWriterForCode(HttpStatusCode code)
+				=> GetLineWriterFor((int)code + "-" + code.ToString());
+			#endregion
 
-            using var httpClient = new HttpClient();
-            var lineCount = 0;
-            var sw = Stopwatch.StartNew();
-            try
-            {
-                await source
-                    .ToChannel(100)
-                    .ReadAllConcurrentlyAsync(200, async line =>
-                    {
-                        AsyncLineWriter resultWriter;
+			using var httpClient = new HttpClient();
+			var lineCount = 0;
+			var sw = Stopwatch.StartNew();
+			try
+			{
+				await source
+					.ToChannel(100)
+					.ReadAllConcurrentlyAsync(200, async line =>
+					{
+						AsyncLineWriter resultWriter;
 
-                        var url = CsvUtility.GetLine(line).ElementAtOrDefault(columnIndex);
-                        if (url == null || url.StartsWith('/') && prefix == null)
-                        {
-                            resultWriter = GetLineWriterFor("skipped");
-                        }
-                        else
-                        {
-                            if (url.StartsWith('/')) url = prefix + url;
-                            var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
-                            var code = response.StatusCode;
+						var url = CsvUtility.GetLine(line).ElementAtOrDefault(columnIndex);
+						if (url == null || url.StartsWith('/') && prefix == null)
+						{
+							resultWriter = GetLineWriterFor("skipped");
+						}
+						else
+						{
+							if (url.StartsWith('/')) url = prefix + url;
+							var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+							var code = response.StatusCode;
 
-                            resultWriter = GetLineWriterForCode(code);
-                        }
+							resultWriter = GetLineWriterForCode(code);
+						}
 
-                        await resultWriter.WriteAsync(line);
+						await resultWriter.WriteAsync(line);
 
-                        var count = Interlocked.Increment(ref lineCount);
-                        if (count % 100 == 0)
-                            lock (sw) Console.WriteLine("Lines Processed: {0:N0}", count);
-                    });
+						var count = Interlocked.Increment(ref lineCount);
+						if (count % 100 == 0)
+							lock (sw) Console.WriteLine("Lines Processed: {0:N0}", count);
+					});
 
-                Console.WriteLine("File read complete: {0:N3} seconds", sw.Elapsed.TotalSeconds);
+				Console.WriteLine("File read complete: {0:N3} seconds", sw.Elapsed.TotalSeconds);
 				source.Close();
-            }
-            finally
-            {
-                await Task.WhenAll(writers.Values.Select(e => e.CompleteAsync()));
-            }
+			}
+			finally
+			{
+				await Task.WhenAll(writers.Values.Select(e => e.CompleteAsync()));
+			}
 
-            Console.WriteLine("Total time: {0:N3} seconds", sw.Elapsed.TotalSeconds);
+			Console.WriteLine("Total time: {0:N3} seconds", sw.Elapsed.TotalSeconds);
 
-        }
-    }
+		}
+	}
 }
